89d356fa86d083e0ab2c5b0839bcd7203cd005a1
nguyentienviet123456 <nguyentienviet123456@gmail.com>
2017-06-01 14:41:54 +0700

    nop bai tap lan 1

--- src/Action/LoginAction.php
+++ src/Action/LoginAction.php
@@ -34,6 +34,49 @@ class LoginAction
         $this->response = $response;
 
         // TODO: write login action code
+        if(isset($_POST['submit'])){
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+        $username = mysqli_escape_string($con,$username);
+        $password = mysqli_escape_string($con,$password);
+
+
+        //Database connection
+        $con = mysqli_connect('localhost','root','Jarious','register_and_login');
+        $error = false;
+
+
+
+        if (!$con) {
+             die("Connection failed: " . mysqli_connect_error());
+        }           
+
+        $userchecker = mysqli_query($con,"SELECT * FROM users WHERE user_name = '$username'");
+
+
+        if(empty($username) || mb_strlen($username) < 5){
+            echo "Username is too short or empty." . '<br />';
+            $error = true;
+
+        }
+
+        if(empty($password) || mb_strlen($password) < 5){
+            echo 'Password is too short or empty. <br />';
+            $error = true;
+        }
+
+        if(mysqli_num_rows($userchecker) > 1){
+            echo 'Username is already taken.';
+            $еrror = true;
+        }
+
+        if(!$error){
+            $password = md5($password);
+            $query = "INSERT INTO users(user_name,pass_word) VALUES ('$username', '$password')";
+            $useradd = mysqli_query($con,$query);
+        }
+
+    }
     }
 
     /**
